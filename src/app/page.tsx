db5cb053a6eb3f8fa4356b9653b349d347eab496
Raul <raul_cosmin_26@yahoo.com>
2025-01-15 21:21:46 +0200

    Add landmark for a11y.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,10 @@ import { useState } from "react";
 export default function Home() {
   const [period, setPeriod] = useState("weekly");
   return (
-    <div className="mx-auto mt-[81px] max-w-[375px] px-6 sm:max-w-[640px] lg:max-w-[1024px] xl:mt-[253px] xl:max-w-[1148px]">
+    <div
+      role="main"
+      className="mx-auto mt-[81px] max-w-[375px] px-6 sm:max-w-[640px] lg:max-w-[1024px] xl:mt-[253px] xl:max-w-[1148px]"
+    >
       <div className="grid grid-cols-1 gap-[30px] sm:grid-cols-2 sm:grid-rows-6 lg:grid-cols-3 lg:grid-rows-3 xl:grid-cols-4 xl:grid-rows-2">
         <Profile setPeriod={setPeriod} period={period} />
         {timeSpent.map((item) => (
